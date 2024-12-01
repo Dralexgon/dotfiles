f62c503077a8e05bb0571bf35ff85737d5f9fb3a
Dralexgon <99817451+Dralexgon@users.noreply.github.com>
2024-12-01 22:52:53 +0100

    first push

---
+++ backup-bashrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOTFILES_DIR=./dotfiles/bash
+
+cp ~/.bashrc $DOTFILES_DIR/bashrc
+
+echo "Bash configuration backed up to $DOTFILES_DIR"
---
+++ restore-bashrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOTFILES_DIR=./dotfiles/bash
+
+cp ~/.bashrc $DOTFILES_DIR/bashrc
+
+echo "bashrc backed up to $DOTFILES_DIR"
