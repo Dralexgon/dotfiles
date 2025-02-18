9d58b1ea44a9f559203bde1101444fb06da5a467
Dralexgon <99817451+Dralexgon@users.noreply.github.com>
2025-02-18 15:52:49 +0100

    cleaning and fix boot issues

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-if [ ! -e nixos/hardware-configuration.nix ]; then
+if [ ! -e nixos/hardware-configuration.nix ] || ! diff nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix; then
+    sudo rm nixos/hardware-configuration.nix # &>/dev/null
     sudo nixos-generate-config --dir nixos
+    sudo cp nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 fi
 
-sudo nixos-rebuild switch --flake .#alex
+sudo nixos-rebuild boot --flake .#alex
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,11 +19,12 @@ in
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.device = "nodev";
-  # boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
