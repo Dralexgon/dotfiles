ddc243e69c6ba2e04f231afdeac33a099cc16e7e
Dralexgon <99817451+Dralexgon@users.noreply.github.com>
2025-02-19 20:06:34 +0100

    comments old scripts

--- scripts/apply-stow.sh
+++ scripts/apply-stow.sh
@@ -2,53 +2,55 @@
 
 DOTFILES_DIR=./dotfiles
 
-if ! command -v stow 2>&1 >/dev/null; then
-    echo "Stow not found, install GNU Stow"
-    exit 1
-fi
+# TODO: rework
 
-# Gnome
+# if ! command -v stow 2>&1 >/dev/null; then
+#     echo "Stow not found, install GNU Stow"
+#     exit 1
+# fi
 
-dconf load /org/gnome/shell/extensions/ <$DOTFILES_DIR/gnome/gnome-shell-extensions.dconf
-dconf load /org/gnome/desktop/interface/ <$DOTFILES_DIR/gnome/gnome-settings.dconf
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ <$DOTFILES_DIR/gnome/keyboard-shortcuts.dconf
+# # Gnome
 
-cd $DOTFILES_DIR/gnome || exit
-mkdir -p "$HOME/.config/autostart" &
-stow --target="$HOME/.config/autostart" autostart
-mkdir -p "$HOME/.themes" &
-stow --target="$HOME/.themes" .themes
-mkdir -p "$HOME/.icons" &
-stow --target="$HOME/.icons" .icons
+# dconf load /org/gnome/shell/extensions/ <$DOTFILES_DIR/gnome/gnome-shell-extensions.dconf
+# dconf load /org/gnome/desktop/interface/ <$DOTFILES_DIR/gnome/gnome-settings.dconf
+# dconf load /org/gnome/settings-daemon/plugins/media-keys/ <$DOTFILES_DIR/gnome/keyboard-shortcuts.dconf
 
-# Hyprland
+# cd $DOTFILES_DIR/gnome || exit
+# mkdir -p "$HOME/.config/autostart" &
+# stow --target="$HOME/.config/autostart" autostart
+# mkdir -p "$HOME/.themes" &
+# stow --target="$HOME/.themes" .themes
+# mkdir -p "$HOME/.icons" &
+# stow --target="$HOME/.icons" .icons
 
-cd ..
-mkdir -p "$HOME/.config/hypr" &
-stow --adopt --target="$HOME/.config/hypr" hypr
-mkdir -p "$HOME/.config/kitty" &
-stow --target="$HOME/.config/kitty" kitty
-mkdir -p "$HOME/.config/rofi" &
-stow --target="$HOME/.config/rofi" rofi
-mkdir -p "$HOME/.config/waybar" &
-stow --target="$HOME/.config/waybar" waybar
+# # Hyprland
 
-# Bashrc
+# cd ..
+# mkdir -p "$HOME/.config/hypr" &
+# stow --adopt --target="$HOME/.config/hypr" hypr
+# mkdir -p "$HOME/.config/kitty" &
+# stow --target="$HOME/.config/kitty" kitty
+# mkdir -p "$HOME/.config/rofi" &
+# stow --target="$HOME/.config/rofi" rofi
+# mkdir -p "$HOME/.config/waybar" &
+# stow --target="$HOME/.config/waybar" waybar
 
-stow --target="$HOME" bash
+# # Bashrc
 
-# Neovim
+# stow --target="$HOME" bash
 
-mkdir -p "$HOME/.config/nvim" &
-stow --target="$HOME/.config/nvim" nvim
+# # Neovim
 
-# Nixos
+# mkdir -p "$HOME/.config/nvim" &
+# stow --target="$HOME/.config/nvim" nvim
 
-if grep -q nixos </etc/os-release; then
-    test -f /etc/nixos/configuration.nix && sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
-    # sudo stow --ignore="hardware-configuration.nix" --target="/etc/nixos" nixos
-    # sudo stow --adopt --target="/etc/nixos" nixos
-    sudo stow --adopt --target="/etc/nixos" nixos
-fi
+# # Nixos
 
-echo "Complete"
+# if grep -q nixos </etc/os-release; then
+#     test -f /etc/nixos/configuration.nix && sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
+#     # sudo stow --ignore="hardware-configuration.nix" --target="/etc/nixos" nixos
+#     # sudo stow --adopt --target="/etc/nixos" nixos
+#     sudo stow --adopt --target="/etc/nixos" nixos
+# fi
+
+# echo "Complete"
--- scripts/create.sh
+++ scripts/create.sh
@@ -2,40 +2,42 @@
 
 DOTFILES_DIR=./dotfiles
 
-mkdir -p $DOTFILES_DIR
+# TODO: rework
 
-# Gnome
+# mkdir -p $DOTFILES_DIR
 
-mkdir -p $DOTFILES_DIR/gnome
-dconf dump /org/gnome/shell/extensions/ >$DOTFILES_DIR/gnome/gnome-shell-extensions.dconf
-dconf dump /org/gnome/desktop/interface/ >$DOTFILES_DIR/gnome/gnome-settings.dconf
-dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >$DOTFILES_DIR/gnome/keyboard-shortcuts.dconf
+# # Gnome
 
-cp -r ~/.config/autostart $DOTFILES_DIR/gnome/autostart
-cp -r ~/.themes $DOTFILES_DIR/gnome/.themes
-cp -r ~/.icons $DOTFILES_DIR/gnome/.icons
+# mkdir -p $DOTFILES_DIR/gnome
+# dconf dump /org/gnome/shell/extensions/ >$DOTFILES_DIR/gnome/gnome-shell-extensions.dconf
+# dconf dump /org/gnome/desktop/interface/ >$DOTFILES_DIR/gnome/gnome-settings.dconf
+# dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >$DOTFILES_DIR/gnome/keyboard-shortcuts.dconf
 
-# Bashrc
+# cp -r ~/.config/autostart $DOTFILES_DIR/gnome/autostart
+# cp -r ~/.themes $DOTFILES_DIR/gnome/.themes
+# cp -r ~/.icons $DOTFILES_DIR/gnome/.icons
 
-mkdir -p $DOTFILES_DIR/bash
-cp ~/.bashrc $DOTFILES_DIR/bash/.bashrc
+# # Bashrc
 
-# Hyprland
+# mkdir -p $DOTFILES_DIR/bash
+# cp ~/.bashrc $DOTFILES_DIR/bash/.bashrc
 
-#mkdir -p $DOTFILES_DIR/hypr $DOTFILES_DIR/kitty $DOTFILES_DIR/rofi $DOTFILES_DIR/waybar
-cp -r ~/.config/hypr $DOTFILES_DIR
-cp -r ~/.config/kitty $DOTFILES_DIR
-cp -r ~/.config/rofi $DOTFILES_DIR
-cp -r ~/.config/waybar $DOTFILES_DIR
+# # Hyprland
 
-# Neovim
+# #mkdir -p $DOTFILES_DIR/hypr $DOTFILES_DIR/kitty $DOTFILES_DIR/rofi $DOTFILES_DIR/waybar
+# cp -r ~/.config/hypr $DOTFILES_DIR
+# cp -r ~/.config/kitty $DOTFILES_DIR
+# cp -r ~/.config/rofi $DOTFILES_DIR
+# cp -r ~/.config/waybar $DOTFILES_DIR
 
-#mkdir -p $DOTFILES_DIR/nvim
-cp -r ~/.config/nvim $DOTFILES_DIR
+# # Neovim
 
-# Nixos
-if grep -q nixos </etc/os-release; then
-    cp /etc/nixos/configuration.nix $DOTFILES_DIR/nixos/configuration.nix
-fi
+# #mkdir -p $DOTFILES_DIR/nvim
+# cp -r ~/.config/nvim $DOTFILES_DIR
 
-echo "Configuration backed up to $DOTFILES_DIR"
+# # Nixos
+# if grep -q nixos </etc/os-release; then
+#     cp /etc/nixos/configuration.nix $DOTFILES_DIR/nixos/configuration.nix
+# fi
+
+# echo "Configuration backed up to $DOTFILES_DIR"
