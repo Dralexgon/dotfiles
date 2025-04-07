aed3a2ae44bcd67e2c47086adcf6e72594999498
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2025-04-07 02:05:08 +0200

    restructure

---
+++ nixos/hosts/nixos-tower-RTX3060/configuration.nix
@@ -0,0 +1,39 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+
+
+    # You probably shouldn't remove it
+    ./../../core.nix
+
+
+    # Main modules
+    ./../../modules/gnome.nix
+    ./../../modules/hyprland.nix
+    ./../../modules/auto-update.nix
+
+    # Hardware specific modules
+    ./../../modules/nvidia-drivers.nix
+    ./../../modules/razer.nix
+    #./../../modules/swap.nix
+
+    # Personal modules
+    ./../../modules/epita.nix
+    ./../../modules/gaming.nix
+    ./../../modules/boot-animation.nix
+    ./../../modules/ricing.nix
+    ./../../modules/ricing-tools.nix
+    ./../../modules/flatpak.nix
+  ];
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+}
