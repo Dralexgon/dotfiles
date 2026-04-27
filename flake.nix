412c21ff8f591cf29bb74b0e3397007c67b41390
Dralexgon <dral3xgon@gmail.com>
2026-04-27 15:25:57 +0200

    update

--- flake.nix
+++ flake.nix
@@ -44,6 +44,15 @@
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        # IMPORTANT: To ensure compatibility with the latest Firefox version, use nixpkgs-unstable.
+        nixpkgs.follows = "nixpkgs";
+        # home-manager.follows = "home-manager";
+      };
+    };
   };
 
 
