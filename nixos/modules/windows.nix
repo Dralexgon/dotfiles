b41d83a6cf49a86da7b9b0fd1416bd6800121d07
Dralexgon <dral3xgon@gmail.com>
2026-06-09 10:36:48 +0200

    remove wrong option

--- nixos/modules/windows.nix
+++ nixos/modules/windows.nix
@@ -5,6 +5,6 @@
   time.hardwareClockInLocalTime = true;
 
   nix.gc.automatic = true;
-  nix.gc.options = ["--delete-older-than 30d"];
+  # nix.gc.options = ["--delete-older-than 30d"];
   boot.loader.systemd-boot.configurationLimit = 2;
 }
