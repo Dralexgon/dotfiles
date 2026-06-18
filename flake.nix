70ab2d351e2cfc5fe8ac1d9b6163f664ca72b2dd
Dralexgon <dral3xgon@gmail.com>
2026-06-18 23:47:31 +0200

    working secure boot !

--- flake.nix
+++ flake.nix
@@ -52,10 +52,17 @@
         # home-manager.follows = "home-manager";
       };
     };
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v1.0.0";
+
+      # Optional but recommended to limit the size of your system closure.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
 
-  outputs = { self, nixpkgs, nvf, ... }@inputs:
+  outputs = { self, nixpkgs, nvf, lanzaboote, ... }@inputs:
   let
     system = "x86_64-linux";
   in
@@ -77,6 +84,25 @@
         nvf.nixosModules.default
 
         # inputs.boot-animation.nixosModules.default
+        lanzaboote.nixosModules.lanzaboote
+        ({ pkgs, lib, ... }: {
+
+          environment.systemPackages = [
+            # For debugging and troubleshooting Secure Boot.
+            pkgs.sbctl
+          ];
+
+          # Lanzaboote currently replaces the systemd-boot module.
+          # This setting is usually set to true in configuration.nix
+          # generated at installation time. So we force it to false
+          # for now.
+          boot.loader.systemd-boot.enable = lib.mkForce false;
+
+          boot.lanzaboote = {
+            enable = true;
+            pkiBundle = "/var/lib/sbctl";
+          };
+        })
       ];
     };
 
