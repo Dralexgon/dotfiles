a99222899a4694db1429d2d30093cae2b057ba17
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2026-06-18 17:40:57 +0200

    add buggy latex

--- nixos/modules/epita/latex.nix
+++ nixos/modules/epita/latex.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let
+  # older way: nixpkgs-stable = inputs.nixpkgs-stable.legacyPackages.${pkgs.system};
+  pkgs-stable = import inputs.nixpkgs-stable { system = pkgs.system; config.allowUnfree = true; };
+in
 {
   environment.systemPackages = with pkgs; [
-    texliveFull
-    fontconfig
+    pkgs-stable.texliveFull
+    pkgs-stable.fontconfig
   ];
 }
 
