18d564d81caf05f91faf9bfbea1b10023327d971
Dralexgon <dral3xgon@gmail.com>
2026-06-19 00:00:42 +0200

    install.sh work in progress

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Work in progress..."
+
+# Ask for the hostname
+# Ask for the username
+# Create a folder in nixos/hosts
+# Copy the configuration.nix to the folder
+# Create hardware-configuration.nix in the folder
+# Modify the flake.nix to include the new host
